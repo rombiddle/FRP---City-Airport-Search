c7053605e89cf1a191514b155f1e1680b06c57f6
Romain <rbrunie@cityway.fr>
2021-06-04 14:50:24 +0200

    Add SearchCityViewController + Storyboard

---
+++ CityAirportSearch/CityAirportSearch/SearchCityViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchCityViewController.swift
+//  CityAirportSearch
+//
+//  Created by Romain Brunie on 04/06/2021.
+//
+
+import UIKit
+
+class SearchCityViewController: UIViewController {
+    
+    @IBOutlet weak var roundedView: UIView!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CityAirportSearch/CityAirportSearch/SearchCityViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchCityViewModel.swift
+//  CityAirportSearch
+//
+//  Created by Romain Brunie on 04/06/2021.
+//
+
+import Foundation
+
+class test: Decodable {
+    
+}
